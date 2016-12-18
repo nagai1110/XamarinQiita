cd92e716a4a93798d55eb79763e0984edcd5647d
nagai1110 <takaya.nagai@jrits.ricoh.co.jp>
2016-12-18 20:26:58 +0900

    Detail画面

---
+++ Qiita/Qiita/Page/ItemListPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Qiita.API;
+
+namespace Qiita.Page
+{
+	public partial class ItemListPage : ContentPage
+	{
+        ObservableCollection<QiitaItem> Items = new ObservableCollection<QiitaItem>();
+
+        public ItemListPage ()
+		{
+			InitializeComponent ();
+
+            ItemList.ItemsSource = Items;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            QiitaAPI api = new QiitaAPI();
+            api.GetAllItems(
+                items =>
+                {
+                    foreach (QiitaItem item in items)
+                    {
+                        Items.Add(item);
+                    }
+                },
+                () =>
+                {
+                    int bbb = 0;
+                });
+        }
+
+    }
+}
