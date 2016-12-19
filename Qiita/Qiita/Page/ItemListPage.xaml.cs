3937a9b131ad24e4cf77989aa18ef88f1234f170
nagai1110 <takaya.nagai@jrits.ricoh.co.jp>
2016-12-20 06:43:32 +0900

    OAuthプロト作成中

--- Qiita/Qiita/Page/ItemListPage.xaml.cs
+++ Qiita/Qiita/Page/ItemListPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Auth;
 using Qiita.API;
 
 namespace Qiita.Page
@@ -25,20 +26,45 @@ namespace Qiita.Page
         {
             base.OnAppearing();
 
-            QiitaAPI api = new QiitaAPI();
-            api.GetAllItems(
-                items =>
-                {
-                    foreach (QiitaItem item in items)
-                    {
-                        Items.Add(item);
-                    }
-                },
-                () =>
-                {
-                    int bbb = 0;
-                });
+            //QiitaAPI api = new QiitaAPI();
+            //api.GetAllItems(
+            //    items =>
+            //    {
+            //        foreach (QiitaItem item in items)
+            //        {
+            //            Items.Add(item);
+            //        }
+            //    },
+            //    () =>
+            //    {
+            //        int bbb = 0;
+            //    });
+
+            var auth = new OAuth2Authenticator(
+                "cdd6590e0e9bc747e989e91720f98e00bbfa7b7d",
+                "read_qiita",
+                new Uri("https://qiita.com/api/v2/oauth/authorize"),
+                new Uri("http://qiita.com/nagasakulllo"));
+
+            auth.AllowCancel = true;
+            auth.Completed += async (object sender, AuthenticatorCompletedEventArgs ee) => {
+
+                // Now that we're logged in, make a OAuth2 request to get the user's info.
+                var accessToken = ee.Account.Properties["access_token"].ToString();
+            };
+
+            var oauther = DependencyService.Get<IQiitaOAuth>();
+
+            auth.Completed += OnAuthenticationCompleted;
+            oauther.StartOAuth(auth);
         }
 
+        async void OnAuthenticationCompleted(object sender, AuthenticatorCompletedEventArgs e)
+        {
+            if (e.IsAuthenticated)
+            {
+
+            }
+        }
     }
 }
